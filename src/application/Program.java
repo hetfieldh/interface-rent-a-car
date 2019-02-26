6b2990eb150aee93136af54fa001ada00220f56d
Douglas Osorio <douglas.anderson7@gmail.com>
2019-02-25 21:11:29 -0300

    Adicionado local USA

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,41 +10,50 @@ import model.entities.CarRental;
 import model.entities.Vehicle;
 import model.services.BrazilTaxService;
 import model.services.RentalService;
+import model.services.UsaTaxService;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy HH:ss");
-		
-		System.out.println("Informe os dados da locacao: ");
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
+
+		System.out.println("Informe os dados da locação: ");
+		System.out.print("País de locação (BR/US): ");
+		String country = sc.nextLine();
 		System.out.print("Modelo do carro: ");
 		String carModel = sc.nextLine();
 		System.out.print("Retirada (dd/mm/aaaa hh:mm): ");
 		Date start = sdf.parse(sc.nextLine());
 		System.out.print("Devolucao (dd/mm/aaaa hh:mm): ");
 		Date finish = sdf.parse(sc.nextLine());
-		
+
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
-		
+
 		System.out.print("Informe o valor/hora da locacao: ");
 		double pricePerHour = sc.nextDouble();
 		System.out.print("Informe o valor da diaria da locacao: ");
 		double pricePerDay = sc.nextDouble();
-		
-		RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
-		
+
+		RentalService rentalService;
+
+		if (country.equals("BR") || country.equals("br")) {
+			rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+		} else {
+			rentalService = new RentalService(pricePerDay, pricePerHour, new UsaTaxService());
+		}
+
 		rentalService.processInvoice(cr);
-		
+
 		System.out.println("---------------------------------");
 		System.out.println("INVOICE");
 		System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
 		System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
 		System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
-		
+
 		sc.close();
 	}
 
--- src/model/services/UsaTaxService.java
+++ src/model/services/UsaTaxService.java
@@ -1,12 +1,12 @@
 package model.services;
 
-public class BrazilTaxService implements TaxService{
-	
+public class UsaTaxService implements TaxService {
+
 	public double tax(double amount) {
 		if (amount <= 100.0) {
-			return amount * 0.2;
+			return amount * 0.1;
 		} else {
-			return amount * 0.15;
+			return amount * 0.075;
 		}
 	}
 }
